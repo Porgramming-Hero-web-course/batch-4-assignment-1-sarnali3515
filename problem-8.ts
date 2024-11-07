20679f816b1ebc417fd1bae883ac88eb7a15e4ef
sarnali15 <191sarnali@gmail.com>
2024-11-08 01:33:39 +0600

    problem-8

---
+++ problem-8.ts
@@ -0,0 +1,15 @@
+{
+
+    function validateKeys<T extends object>(obj: T, keys: (keyof T)[]): boolean {
+        for (let key of keys) {
+            if (!(key in obj)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    const person = { name: "Alice", age: 25, email: "alice@example.com" };
+    console.log(validateKeys(person, ["name", "age"]));
+
+}
